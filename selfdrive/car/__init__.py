3edbd55762d8116f911f44cf129e0329e113071a
Vehicle Researcher <user@comma.ai>
2018-03-18 22:22:31 +0000

    Revert "Can messages captured"
    This reverts commit 7d7b02fe84ffb9cea889882d644fc90699481c83.

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -35,7 +35,7 @@ interfaces = {
   TOYOTA.RAV4H: ToyotaInterface,
   TOYOTA.COROLLA: ToyotaInterface,
   TOYOTA.LEXUS_RXH: ToyotaInterface,
-  TOYOTA.LEXUS_GS: ToyotaInterface,
+  TOYOTA.LEXUS_GS: ToyotaInterface
 
   "simulator2": Sim2Interface,
   "mock": MockInterface
